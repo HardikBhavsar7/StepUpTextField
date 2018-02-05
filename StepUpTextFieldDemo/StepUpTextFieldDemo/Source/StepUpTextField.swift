dbc506ae228f49c6bde94e39398988eac253c440
HardikBhavsar7 <33711890+HardikBhavsar7@users.noreply.github.com>
2018-02-05 15:49:33 +0530

    After structural changes

--- StepUpTextFieldDemo/StepUpTextFieldDemo/Source/StepUpTextField.swift
+++ StepUpTextFieldDemo/StepUpTextFieldDemo/Source/StepUpTextField.swift
@@ -119,3 +119,110 @@ class StepUpTextField: UITextField, UITextFieldDelegate {
         }
     }
 }
+
+// MARK: - Common Extensions
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set(newValue) {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+    /// The color of the shadow. Defaults to opaque black. Colors created from patterns are currently NOT supported. Animatable.
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.masksToBounds =  false
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    /// The opacity of the shadow. Defaults to 0. Specifying a value outside the [0,1] range will give undefined results. Animatable.
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    /// The shadow offset. Defaults to (0, -3). Animatable.
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    /// The blur radius used to create the shadow. Defaults to 3. Animatable.
+    @IBInspectable var shadowRadius: Double {
+        get {
+            return Double(self.layer.shadowRadius)
+        }
+        set {
+            self.layer.shadowRadius = CGFloat(newValue)
+        }
+    }
+}
+
+extension String {
+    
+    func chunkFormatted(withChunkSize chunkSize: Int,
+                        withSeparator separator: Character = " ") -> String {
+        return characters.filter { $0 != separator }.chunk(n: chunkSize)
+            .map{ String($0) }.joined(separator: String(separator))
+    }
+}
+
+extension Collection {
+    public func chunk(n: IndexDistance) -> [SubSequence] {
+        var res: [SubSequence] = []
+        var i = startIndex
+        var j: Index
+        while i != endIndex {
+            j = index(i, offsetBy: n, limitedBy: endIndex) ?? endIndex
+            res.append(self[i..<j])
+            i = j
+        }
+        return res
+    }
+}
+
+
